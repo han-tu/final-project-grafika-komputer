af76a8299fa9b436b5c74a521467d188a0cfff4a
Raihan Alifianto <purplelamborghini24@gmail.com>
2021-11-15 23:10:04 +0700

    Alip 3 pull

--- object/wall.js
+++ object/wall.js
@@ -1,10 +1,15 @@
 import * as THREE from 'https://threejsfundamentals.org/threejs/resources/threejs/r132/build/three.module.js';
 
-function createBox(){
-    const geometryWall = new THREE.BoxGeometry(1, 1, 1); //ukuran shape box disesuaikan
-    const materialWall = new THREE.MeshPhongMaterial({color: 0xFF3E2723, metalness: 0.5, roughness: 0.1});
+export function createBox(boxSize, boxColor){
+    const geometryWall = new THREE.BoxGeometry(boxSize, boxSize, boxSize);
+    const materialWall = new THREE.MeshPhongMaterial(
+        {
+            color: boxColor, 
+            metalness: 0.5, 
+            roughness: 0.1
+        });
     const wall = new THREE.Mesh(geometryWall, materialWall);
-    scene.add(wall);
+    return wall;
 }
 
 
