855496d473cab8e17832168d4445c9ab1d6190c4
Han <haann.m5@gmail.com>
2021-11-15 23:09:08 +0700

    Han 4
    Rapihin kodingan

---
+++ object/initialGraphics/pointerLock.js
@@ -0,0 +1,23 @@
+import { PointerLockControls } from 'https://threejsfundamentals.org/threejs/resources/threejs/r132/examples/jsm/controls/PointerLockControls.js';
+
+export function createControls(camera, body, blocker, instructions, scene) {
+    let controls = new PointerLockControls(camera, body)
+
+    instructions.addEventListener('click', function () {
+        controls.lock();
+    });
+
+    controls.addEventListener('lock', function () {
+        instructions.style.display = 'none';
+        blocker.style.display = 'none';
+    });
+
+    controls.addEventListener('unlock', function () {
+        blocker.style.display = 'block';
+        instructions.style.display = '';
+    });
+
+    scene.add(controls.getObject());
+
+    return controls;
+}
