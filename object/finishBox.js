715fca064c6cf71e1b902894224041c5db6ab18b
Han <haann.m5@gmail.com>
2021-12-05 01:06:56 +0700

    Han 5
    Menambahkan Day Night Cycle
    Menambahkan Finish
    Menambahkan Maze

--- object/finishBox.js
+++ object/finishBox.js
@@ -1,12 +1,15 @@
 import * as THREE from 'https://threejsfundamentals.org/threejs/resources/threejs/r132/build/three.module.js';
 
-export function createBox(boxSize, boxColor){
-    const geometryWall = new THREE.BoxGeometry(boxSize, 100, boxSize);
+export function createFinishBox(boxSize, boxColor, locX, locZ, scene, finish){
+    const geometryWall = new THREE.BoxGeometry(boxSize, boxSize, boxSize);
     const materialWall = new THREE.MeshPhongMaterial(
         {
             color: boxColor
         });
     const wall = new THREE.Mesh(geometryWall, materialWall);
+    wall.position.set(locX, -5, locZ);
+    scene.add(wall);
+    finish.push(wall);
     return wall;
 }
 
--- object/initialGraphics/camera.js
+++ object/initialGraphics/camera.js
@@ -1,7 +1,7 @@
 import * as THREE from 'https://threejsfundamentals.org/threejs/resources/threejs/r132/build/three.module.js';
 
 export function createCamera(window) {
-    let camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 1000);
+    let camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 2000);
     camera.position.y = 10;
     return camera;
 }
